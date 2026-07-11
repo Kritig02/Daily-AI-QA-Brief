aa6ac6b7cdc84000e75ed0db32604ecee1e42d78
Kriti Gupta <kritig92@gmail.com>
2026-07-11 12:24:14 -0400

    fixed message formatting

--- telegram_bot.py
+++ telegram_bot.py
@@ -10,4 +10,4 @@ CHAT_ID = os.getenv("CHAT_ID")
 
 async def send_message(message):
     bot = Bot(token=BOT_TOKEN)
-    await bot.send_message(chat_id = int(CHAT_ID), text = message, parse_mode="Markdown")
+    await bot.send_message(chat_id = int(CHAT_ID), text = message)
